ffbcbbb6c66fd1d3230250220d20fe7025f08d34
Sothus <krzysiek651@gmail.com>
2018-03-10 17:08:32 +0100

    Modification of project naming

--- super_auctionhouse/urls.py
+++ super_auctionhouse/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from homepage import views
+from auctionhouse import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name="home"),
--- super_auctionhouse/wsgi.py
+++ super_auctionhouse/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for AuctionHouse project.
+WSGI config for super_auctionhouse project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AuctionHouse.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "super_auctionhouse.settings")
 
 application = get_wsgi_application()
