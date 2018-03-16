ed2597573f594f71224d5e9471692c33ce9d50aa
Sothus <krzysiek651@gmail.com>
2018-03-16 10:44:32 +0100

    Updated requirement list - installed Pillow. Created Product model

--- auctionhouse/models.py
+++ auctionhouse/models.py
@@ -14,6 +14,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products')
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    descritpion = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) 
 
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
