ceeffe74f03cbe0e63f794076e214e9a3cbc9f0f
Sothus <krzysztof.plucinski.95@gmail.com>
2018-04-13 11:35:13 +0200

    Removed problem with disconnecting users

--- auctionhouse/routing.py
+++ auctionhouse/routing.py
@@ -3,6 +3,6 @@ from auctionhouse.consumer import ws_message, ws_connect, ws_disconnect
 channel_routing = {
 	'websocket.connect': ws_connect,
 	'websocket.receive': ws_message,
-	'webspcket.disconnect': ws_disconnect,
+	'websocket.disconnect': ws_disconnect,
 
 }
