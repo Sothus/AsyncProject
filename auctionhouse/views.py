1848a05ee9cb8382034ad57b193a8feecd0d6dc0
Sothus <krzysztof.plucinski.95@gmail.com>
2018-04-27 11:31:35 +0200

    Still preparing user authentication system

--- auctionhouse/views.py
+++ auctionhouse/views.py
@@ -25,3 +25,24 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug)
     return render(request, 'product/detail.html', {'product': product})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_vald():
+            cd = form.cleaned_data
+            user = authenticate(username = cd['username'],
+                                password = cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated "\
+                                        "successfully")
+                else:
+                    return HttpResponse("Disabled account")
+            else:
+                return HttpResponse("Invalid login")
+        else:
+            form = LoginForm()
+        return render(request, "login.html", {'form': form})
